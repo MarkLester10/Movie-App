916146b04d041b6f0a287bded7449580791cc930
Mark Lester Morta <marklester10.mlsm@gmail.com>
2020-11-02 14:29:05 +0800

    edit .env

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/main.css">
+    <link rel="icon" href="/imgs/logo.png"/>
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
     <livewire:styles>
